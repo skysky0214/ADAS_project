a02cac5a2258c82572059698d970112a1569eadf
khj <khj@local>
2026-06-08 15:00:24 +0900

    Stop tracking ROS2 build artifacts

--- ros2/build/hesai_ros_driver/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/khj/25project_ws/ros2/src/HesaiLidar_ROS_2.0
-# Build directory: /home/khj/25project_ws/ros2/build/hesai_ros_driver
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("hesai_ros_driver__py")
-subdirs("src/driver/HesaiLidar_SDK_2.0")
